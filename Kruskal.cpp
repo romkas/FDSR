056120b346024f62b280043d4ca785c44e02b63a
rome <romekulum2308@gmail.com>
2017-03-31 14:38:06 +0300

    commit #3

--- Kruskal.cpp
+++ Kruskal.cpp
@@ -17,34 +17,13 @@ namespace graph
 	template <class T>
 	float ImageGraph<T>::calc_weigth(datastruct::Node<T> *n1, datastruct::Node<T> *n2, int im_type, bool use_distance)
 	{
-		cv::Vec<
-		if (im_type == CV_32F)
-		{
-			if (use_distance)
-			{
-				return cv::sqrt((n1->value[0] - n2->value[0])*(n1->value[0] - n2->value[0]) +
-					(n1->xcoord - n2->xcoord)*(n1->xcoord - n2->xcoord) +
-					(n1->ycoord - n2->ycoord)*(n1->ycoord - n2->ycoord));
-			}
-			else
-			{
-				return cv::abs(n1->value[0] - n2->value[0]);
-			}
-		}
-		else // CV_32F3
-		{
-			cv::Vec3f rgb = n1->value - n2->value;
-			if (use_distance)
-			{
-				return cv::sqrt(rgb.dot(rgb) +
-					(n1->xcoord - n2->xcoord)*(n1->xcoord - n2->xcoord) +
-					(n1->ycoord - n2->ycoord)*(n1->ycoord - n2->ycoord))
-			}
-			else
-			{
-				return cv::sqrt(rgb.dot(rgb));
-			}
-		}
+		cv::Vec<float, (im_type == CV_32F) ? 1 : 3> v;
+		v = n1->value - n2->value;
+		if (use_distance)
+			return cv::sqrt(v.dot(v) + (n1->xcoord - n2->xcoord)*(n1->xcoord - n2->xcoord) +
+				(n1->ycoord - n2->ycoord)*(n1->ycoord - n2->ycoord));
+		else
+			return cv::sqrt(v.dot(v));
 	}
 
 	template <class T>
@@ -55,8 +34,13 @@ namespace graph
 		for (int i = 0; i < h; i++)
 			for (int j = 0; j < w; j++)
 			{
-				
 				vertices.MakeSet(image.at(j, i), j, i);
+				edges
+				if (i == 0)
+				{
+
+				}
+				
 
 			}
 	}
