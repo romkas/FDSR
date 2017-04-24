ec3860ff342e6230554220f1e295ab5e770bddb3
rome <romekulum2308@gmail.com>
2017-04-24 08:48:40 +0300

    qqq

--- modelFitting.h
+++ modelFitting.h
@@ -5,8 +5,23 @@
 
 namespace model
 {
-	int ransac(Segment *pseg, int param_n, int param_k, float param_thres, int param_d)
+	cv::RNG ransac_random;
+
+	void _compute_plane(cv::Mat &pointlist, cv::Vec4f &normal)
 	{
+
+	}
+
+	int ransac(cv::Mat &pointlist, int param_n, int param_k, float param_thres, int param_d)
+	{
+		cv::Mat subsample(cv::Size(param_n, 1), CV_32SC1);
+		int sample_size = pointlist.cols;
+		float err;
 		
+		for (int t = 0; t < param_k; t++)
+		{
+			ransac_random.fill(subsample, cv::RNG::UNIFORM, 0, sample_size);
+
+		}
 	}
 };
